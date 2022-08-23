a582d36d4afb09246d9ba00433c596f36a8e1503
Shahriar Ahmmed <shahariarahmmed.sa@gmail.com>
2022-08-23 23:13:27 +0600

    selction functionality

---
+++ js/select_players.js
@@ -0,0 +1,72 @@
+document.getElementById('player-1').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-1-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-2').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-2-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-3').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-3-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-4').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-4-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-5').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-5-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-6').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-6-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-7').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-7-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-8').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-8-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
+document.getElementById('player-9').addEventListener('click', function(){
+        const playerNameSelect = document.getElementById('player-9-name').innerText;
+        // console.log(playerNameSelect)
+        const nameList = document.getElementById('list');
+        let newListItem = document.createElement('li');
+        newListItem.textContent = playerNameSelect;
+        nameList.appendChild(newListItem);
+})
